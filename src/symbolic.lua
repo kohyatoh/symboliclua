f6c9a664eb646f880946a11080d679cd042034b0
Kohsuke Yatoh <kohyatoh@gmail.com>
2015-03-01 23:28:59 +0900

    add z3py code emission

--- src/symbolic.lua
+++ src/symbolic.lua
@@ -17,6 +17,25 @@ local function gettype(v)
     return issymbol(v) and v.t or type(v)
 end
 
+local function z3code(symbols, constraints)
+    local lines = {}
+    table.insert(lines, "from z3 import *")
+    table.insert(lines, "solver = Solver()")
+    for i, v in ipairs(symbols) do
+        table.insert(lines, string.format("%s = Real('%s')", v, v))
+    end
+    for i, v in ipairs(constraints) do
+        table.insert(lines, string.format("solver.add(%s)", v))
+    end
+    table.insert(lines, "r = solver.check()")
+    table.insert(lines, "print(r)")
+    table.insert(lines, "if repr(r) == 'sat':")
+    table.insert(lines, "   m = solver.model()")
+    table.insert(lines, "   for i in range(len(m)):")
+    table.insert(lines, "       print('%s,%s' % (m[i], m[m[i]]))")
+    return table.concat(lines, "\n")
+end
+
 -- package functions
 function symbolic.eval (f)
     for k = 0, 100 do
@@ -24,12 +43,8 @@ function symbolic.eval (f)
         constraints = {}
         local r = pcall(f)
         if r then
-            for i, v in ipairs(symbols) do
-                print(string.format("v %s: %s", v, v.t))
-            end
-            for i, v in ipairs(constraints) do
-                print(string.format("c %s", v))
-            end
+            local a = z3code(symbols, constraints)
+            print(a)
             break
         end
     end
@@ -69,7 +84,7 @@ function Symbol.__add (a, b)
     if issymbol(a) then a:settype("number") end
     if issymbol(b) then b:settype("number") end
     newval:settype("number")
-    local expr = string.format("%s=%s+%s", tostring(newval), tostring(a), tostring(b))
+    local expr = string.format("%s==%s+%s", tostring(newval), tostring(a), tostring(b))
     table.insert(constraints, expr)
     return newval
 end
