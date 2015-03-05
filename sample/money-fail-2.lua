7d494f75101899377f1807a8a28f6dc8f21e648c
Kohsuke Yatoh <kohyatoh@gmail.com>
2015-03-05 17:27:07 +0900

    add fail sample

---
+++ sample/money-fail-2.lua
@@ -0,0 +1,16 @@
+local Dollar = ?
+Dollar.__index = Dollar
+function Dollar:new (amount)
+    return setmetatable({amount = amount}, Dollar)
+end
+function Dollar:amount ()
+    return self.amount
+end
+local Franc = ?
+
+local five = Dollar:new(5)
+local five_f = Franc:new(5)
+assert(five:amount() == 5)
+assert(five:times(2):amount() == 10)
+assert(five_f:amount() == 5)
+assert(five_f:equals(five:times(2)))
